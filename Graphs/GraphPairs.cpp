d5dbfcffaf1166790e8af7e17efd77feb6832947
kangpahul9 <pahulpreet2959@gmail.com>
2025-10-28 15:05:11 +1030

    l

---
+++ Graphs/GraphPairs.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<utility>
+#include<queue>
+
+using namespace std;
+class Graph
+{
+private:
+    int V;
+    list<pair<int,int> > * l;
+public:
+    Graph(int V){
+        this->V=V;
+        l = new list<pair<int,int> > [V];
+    }
+    void addEdge(int u, int v,int wt){
+        l[u].push_back({v,wt});
+        l[v].push_back({u,wt});
+    }    
+    void printList(){
+        for (int u = 0; u < V; u++)
+        {
+        list<pair<int,int> > neighbours = l[u];
+        cout << u << ": ";
+        for (pair<int,int> v : neighbours)
+        {
+            cout << "("<<v.first << ", "<<v.second<<")";
+        }
+        cout<<endl;
+        }
+    }
+    void bfs(){
+        queue<pair<int,int> > q;
+        vector<bool>visited(V,false);
+       q.push({0,0});
+        visited[0]=true;
+        while (!q.empty())
+        {
+            pair<int, int> currPair=q.front();
+            int u = currPair.first;
+            q.pop();
+        cout << "visiting index " << u<<endl;
+            for (auto neighbor : l[u])
+            {
+                if (!(visited[neighbor.first]))
+                {
+                    visited[neighbor.first]=true;
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+    void dfs(pair<int,int> u, vector<bool> &visited){
+        if (visited[u.first])
+        {
+            return;
+        }
+        visited[u.first]=true;
+        cout << "visiting : " << u.first<<endl;
+        list<pair<int,int> > neighbors = l[u.first];
+        for (auto neighbor : neighbors){
+            if (!(visited[neighbor.first]))
+                {
+                    dfs(neighbor,visited);
+                }
+        }
+        
+    }
+};
+
+
+int main(){
+    Graph g(5);
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 2, 1);
+    g.addEdge(1, 3, 1);
+    g.addEdge(2, 4, 1);
+
+    cout << "DFS Traversal:\n";
+    vector<bool> visited(5, false);
+    g.dfs({0,0}, visited);
+
+    cout << "\nBFS Traversal:\n";
+    g.bfs();
+
+    return 0;
+}
