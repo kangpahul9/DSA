fd1a00f961ce6b813f9835c56e7fb1c983b752dc
kangpahul9 <pahulpreet2959@gmail.com>
2025-12-13 14:16:50 +1030

    l

--- Greedy/ActivitySelection.cpp
+++ Greedy/ActivitySelection.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
 #include<vector>
+#include <algorithm>
 using namespace std;
 
+bool compare(pair<int,int> p1, pair<int,int> p2){
+  return p1.second < p2.second;
+
+}
 int maxActivities(vector<int> start,vector<int> end){
+  vector<pair<int,int>> act(start.size(),make_pair(0,0));
+  for (int i = 0; i < start.size(); i++)
+  {
+    act[i]=make_pair(start[i],end[i]);
+  } 
+    sort(act.begin(),act.end(),compare);
+
     int ans = 1;
-    int currEnd = end[0];
+    int currEnd = act[0].second;
     for (int i = 1; i < start.size(); i++)
     {
-       if (start[i]>=currEnd)
+       if (act[i].first>=currEnd)
        {
          ans++;
-         currEnd=end[i];
+         currEnd=act[i].second;
        }
        
     }
@@ -18,8 +30,8 @@ int maxActivities(vector<int> start,vector<int> end){
 }
 
 int main() {
-    vector<int> start = {1, 3, 0, 5, 8, 5};
-    vector<int> end   = {2, 4, 6, 7, 9, 9}; // already sorted by end time
+    vector<int> start = {5, 1, 3, 0, 5, 8};
+    vector<int> end   = {7, 2, 4, 6, 9, 9};
 
     cout << "Maximum number of activities: "
          << maxActivities(start, end) << endl;
